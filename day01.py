fa422434a8f742fa5527c83f3206cd35b9018aad
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2019-11-30 10:25:34 -0800

    Prepare for day 1

---
+++ day01.py
@@ -0,0 +1,14 @@
+"""Solution do day 1 of Advent of Code"""
+
+from get_input import get_input, line_parser
+
+def part1(lines):
+    raise Exception("Not implemented")
+
+def part2(lines):
+    raise Exception("Not implemented")
+
+if __name__ == "__main__":
+    LINES = line_parser(get_input(day=1, year=2019)) 
+    print(f"Part 1: {part1(LINES)}")
+    print(f"Part 2: {part2(LINES)}")
